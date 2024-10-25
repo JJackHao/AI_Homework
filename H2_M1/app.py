cb0dcb8a8497aeaabb4a507f529e549aa81dc600
JJackHao <xhao43@gatech.edu>
2024-10-24 20:00:06 -0400

    H2_M1

--- H2_M1/app.py
+++ H2_M1/app.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from dotenv import load_dotenv
+from model import runModels
+import os
+from werkzeug.utils import secure_filename
 
 load_dotenv()
 
@@ -7,11 +10,25 @@ app =Flask(__name__, template_folder = 'templates', static_folder='static',stati
 
 @app.route('/')
 def index():
-    return render_template('about.html')
+    return render_template('about.html', active = 'index')
 
-@app.route('/milestone1')
+@app.route('/milestone1', methods=["GET","POST"])
 def interaction_1():
-    return render_template('milestone1.html', active='interaction_1')
+    if request.method == 'POST':
+        f = request.files["imgFile"]
+        file_name = secure_filename(f.filename)
+
+        cwd = os.getcwd()
+        upld_path = cwd+'/static/imgs/'+file_name
+        img_path = 'imgs/'+file_name
+        f.save(upld_path)
+
+        (caption, story) = runModels(upld_path)
+        
+
+        return render_template('milestone1.html', active='interaction_1', imgPath=img_path, story=story, caption=caption)
+    else:
+        return render_template('milestone1.html', active='interaction_1')
 
 
 if __name__ == '__main__':
